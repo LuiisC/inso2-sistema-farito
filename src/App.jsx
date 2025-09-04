86dcf254359fff33e2786b2925395f14c7893189
Luis <luiscara94@gmail.com>
2025-09-04 19:10:47 -0300

    Develop: Estructura pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //import './App.css'
-import MainLayout from './pages/MainLayout.jsx';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="equipos" element={<div>Equipos</div> } />
-          <Route path="reparaciones" element={<div>Registrar Reparación</div>} />
+        <Route path="/" element={<Home />}>
+          <Route path="registro" element={<div>Registro</div> } />
+          <Route path="stock" element={<div>Stock</div>} />
+          <Route path="solicitud" element={<div>Solicitud</div>} />
           <Route path="informes" element={<div>Informes</div>} />
+          <Route path="alta" element={<div>Alta</div>} />
           <Route path="logout" element={<div>Cerrando sesión...</div>} />
         </Route>
         <Route path="/login" element={<div>Página de Login</div>} />
